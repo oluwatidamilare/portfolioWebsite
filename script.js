e2ea46e810a201ff0d383e2da52de1156193c16b
David-Oluwatidamilare <david.damilare008@gmail.com>
2022-08-10 11:19:29 +0100

    11:19 10-08-2022

--- script.js
+++ script.js
@@ -42,6 +42,13 @@ $(document).ready(function(){
         loop: true
     });
 
+    var typed = new Typed(".typing-3", {
+        strings: ["Damilare David Adesanya"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
     //owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
